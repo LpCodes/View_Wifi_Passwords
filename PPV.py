81b591bdf4ed0d2aaaeddcf4ccdfa45b4cdd9482
LP <lovelesh_p@zohomail.in>
2023-03-19 17:56:19 +0530

    Update PPV.py

--- PPV.py
+++ PPV.py
@@ -1,24 +1,26 @@
 import subprocess
 
-root = subprocess.check_output(
-    "netsh wlan show profiles", shell=True, stderr=subprocess.STDOUT
-).decode()
-# print(resp)
-
-uname = []
-for x in root.splitlines():
-    if "User" in x:
-
-        uname.append(x.split(":", 1)[-1])
-
-print(uname)
-
-for y in uname:
-    yy = "netsh wlan show profile %s key=Clear" % y
+def main():
+    # Get the list of wireless profiles
+    command = "netsh wlan show profiles"
     try:
-        op = subprocess.check_output(yy, shell=True, stderr=subprocess.STDOUT).decode()
-        print(op)
-    except subprocess.CalledProcessError:
-        print("oops", y)
+        result = subprocess.run(command, shell=True, check=True, capture_output=True, text=True)
+    except subprocess.CalledProcessError as exc:
+        print(f"Command '{command}' failed with error code {exc.returncode}: {exc.stderr}")
+        return
+    profiles = [line.split(":")[1].strip() for line in result.stdout.splitlines() if "All User Profile" in line]
 
-input("Press Any Key to Exit\n")
+    # Get the Wi-Fi passwords for each profile
+    for profile in profiles:
+        command = f"netsh wlan show profile \"{profile}\" key=clear"
+        try:
+            result = subprocess.run(command, shell=True, check=True, capture_output=True, text=True)
+        except subprocess.CalledProcessError as exc:
+            print(f"Command '{command}' failed with error code {exc.returncode}: {exc.stderr}")
+            continue
+        print(result.stdout)
+
+    input("Press Any Key to Exit\n")
+
+if __name__ == "__main__":
+    main()
