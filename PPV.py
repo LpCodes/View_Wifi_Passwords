d0707dc20c05aa0e548d49c03a48f1b46758f373
LP <lovelesh_p@zohomail.in>
2023-10-01 21:29:12 +0530

    updated

--- PPV.py
+++ PPV.py
@@ -1,32 +1,44 @@
 import subprocess
 
 
-def main():
-    # Get the list of wireless profiles
-    command = "netsh wlan show profiles"
+def get_wifi_profiles():
     try:
+        command = "netsh wlan show profiles"
         result = subprocess.check_output(command, shell=True, text=True)
+        return [line.split(":")[1].strip() for line in result.splitlines() if "All User Profile" in line]
     except subprocess.CalledProcessError as exc:
         print(f"Command '{command}' failed with error code {exc.returncode}: {exc.stderr}")
-        return
-    profiles = [line.split(":")[1].strip() for line in result.splitlines() if "All User Profile" in line]
+        return []
 
-    # Get the Wi-Fi passwords for each profile
-    for profile in profiles:
+
+def get_wifi_password(profile):
+    try:
         command = f"netsh wlan show profile \"{profile}\" key=clear"
-        try:
-            result = subprocess.check_output(command, shell=True, text=True)
-        except subprocess.CalledProcessError as exc:
-            print(f"Command '{command}' failed with error code {exc.returncode}: {exc.stderr}")
-            continue
+        result = subprocess.check_output(command, shell=True, text=True)
+        username, password = None, None
         for line in result.splitlines():
             if "SSID" in line:
                 username = line.split(":")[1].strip()
             elif "Key Content" in line:
                 password = line.split(":")[1].strip()
-        print(f"UserName - {username} | PassWord -  {password}")
+        return username, password
+    except subprocess.CalledProcessError as exc:
+        print(f"Command '{command}' failed with error code {exc.returncode}: {exc.stderr}")
+        return None, None
 
-    input("Press Any Key to Exit\n")
+
+def main():
+    wifi_profiles = get_wifi_profiles()
+    if not wifi_profiles:
+        print("No wireless profiles found.")
+        return
+
+    for profile in wifi_profiles:
+        username, password = get_wifi_password(profile)
+        if username is not None and password is not None:
+            print(f"UserName - {username} | PassWord - {password}")
+
+    input("Press Enter to exit\n")
 
 
 if __name__ == "__main__":
